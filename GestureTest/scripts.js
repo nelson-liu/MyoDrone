62d8e80ed54ec51ed37a7901bf3769a0e43eeb96
Nelson Liu <nelson.liu.2009@gmail.com>
2015-01-17 02:57:20 -0500

    basic gesturetest

---
+++ GestureTest/scripts.js
@@ -0,0 +1,35 @@
+var Myo = require('myo');
+var myMyo = Myo.create();
+myMyo.on('fist', function(edge){
+  myMyo.timer(edge, 500, function(){
+    alert('you just made a fist');
+  })
+});
+
+myMyo.on('fingers_spread', function(edge){
+  //Edge is true if it's the start of the pose, false if it's the end of the pose
+  myMyo.timer(edge, 500, function(){
+    alert('you just spread your fingers');
+  })
+});
+
+myMyo.on('wave_in', function(edge){
+  //Edge is true if it's the start of the pose, false if it's the end of the pose
+  myMyo.timer(edge, 500, function(){
+    alert('you just waved in');
+  })
+});
+
+myMyo.on('wave_out', function(edge){
+  //Edge is true if it's the start of the pose, false if it's the end of the pose
+  myMyo.timer(edge, 500, function(){
+    alert('you just waved out');
+  })
+});
+
+myMyo.on('thumb_to_pinky', function(edge){
+  //Edge is true if it's the start of the pose, false if it's the end of the pose
+  myMyo.timer(edge, 500, function(){
+    alert('you just thumb to pinkied');
+  })
+});
