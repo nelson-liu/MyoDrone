5bb3ae1147ef8bd3e538d6877667da52a73c19d9
fc1698 <frankchen1698@gmail.com>
2015-01-17 13:54:29 -0500

    updated GestureDroneTest

--- GestureDroneTest/scripts.js
+++ GestureDroneTest/scripts.js
@@ -1,12 +1,15 @@
 var arDrone = require('ar-drone');
 var client = arDrone.createClient();
-//client.config('general:navdata_demo', 'FALSE');
+client.config('general:navdata_demo', 'FALSE');
 var Myo = require('myo');
 var myMyo = Myo.create();
 //var altitude = navdata.demo.altitudeMeters;
 //var nextaltitude = altitude + 500; //add 500 mm, or .50 m to height
 
+client.on('navdata', function(data){
+    var altitude = navdata.demo.altitudeMeters;
 
+});
 //Edge is true if it's the start of the pose, false if it's the end of the pose
 
 myMyo.on('fingers_spread', function(edge){
@@ -23,31 +26,31 @@ myMyo.on('fist', function(edge){
   })
 });
 
-// myMyo.on('wave_in', function(edge){
-//   myMyo.timer(edge, 500, function(){
-//     console.log('wave in detected');
-//     altitude = navdata.demo.altitudeMeters;
-//     nextaltitude = altitude + 500; //add 500 mm, or .50 m to height
-//     while (altitude !== nextaltitude) {
-//       client.up(0.3);
-//       altitude = navdata.demo.altitudeMeters;
-//     }
-//     client.stop();
-//   })
-// });
+myMyo.on('wave_in', function(edge){
+  myMyo.timer(edge, 500, function(){
+    console.log('wave in detected');
+    altitude = navdata.demo.altitudeMeters;
+    var nextaltitude = altitude + 500; //add 500 mm, or .50 m to height
+    while (altitude !== nextaltitude) {
+      client.up(0.3);
+      altitude = navdata.demo.altitudeMeters;
+    }
+    client.stop();
+  })
+});
 
-// myMyo.on('wave_out', function(edge){
-//   myMyo.timer(edge, 500, function(){
-//     console.log('wave in detected');
-//     altitude = navdata.demo.altitudeMeters;
-//     nextaltitude = altitude - 500; //subtract 500 mm, or .50 m to height
-//     while (altitude !== nextaltitude) {
-//       client.down(0.3);
-//       altitude = navdata.demo.altitudeMeters;
-//     }
-//     client.stop();
-//   })
-// });
+myMyo.on('wave_out', function(edge){
+  myMyo.timer(edge, 500, function(){
+    console.log('wave in detected');
+    altitude = navdata.demo.altitudeMeters;
+    var nextaltitude = altitude - 500; //subtract 500 mm, or .50 m to height
+    while (altitude !== nextaltitude) {
+      client.down(0.3);
+      altitude = navdata.demo.altitudeMeters;
+    }
+    client.stop();
+  })
+});
 //experimental features
 
 // myMyo.on('orientation', function(data){
